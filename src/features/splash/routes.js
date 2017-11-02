d57b5cb292dd063e9e6900ae6b488e4e2e5e2d91
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-11-02 10:05:09 +0100

    Basic template with esnext, mobx, router

--- src/features/splash/routes.js
+++ src/features/splash/routes.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
+import Splash from './screen';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Scene key="root">
-          <Scene key="home" component={Home}/>
+          <Scene key="splash" component={Splash} />
         </Scene>
       </Router>
-    )
+    );
   }
 }
+
+export default App;
